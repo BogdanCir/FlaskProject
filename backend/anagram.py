78df94c2eba07bdb4386734d8d30b548b8088665
BogdanCir <bogdancirciu1@gmail.com>
2026-03-29 20:44:26 +0300

    added sqlite database using sqlalchemy and flask_sqlalchemy (commented) libraries

--- backend/anagram.py
+++ backend/anagram.py
@@ -10,7 +10,10 @@ def anagrams(strs):
             resolve[''.join(sorted(word))] = [word]
     return list(resolve.values())
 
-
+# here we calculate the key of the string. If we have the same key, we return the response directly from the db
+def calculate_key(words):
+    return "".join(sorted(w.lower() for w in words))
 # print(sorted("ana"))
+# print(calculate_key(["ana", "mircea", "gel", "leg", "elg", "aan"]))
 # print(anagrams(["ana", "mircea", "gel", "leg", "elg", "aan"]))
 
