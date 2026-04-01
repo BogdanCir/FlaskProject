a628b2bad3a2b881073e56810bb8fb6880f2e85e
BogdanCir <bogdancirciu1@gmail.com>
2026-04-01 19:59:13 +0300

    add cors origin

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -4,7 +4,7 @@ from app.db import db_session
 
 def create_app(testing=False):
     app = Flask(__name__)
-    CORS(app, origins=["https://flask-backend-production-c1c9.up.railway.app"])
+    CORS(app, origins=["https://flaskproject-production-frontend.up.railway.app"])
 
     if testing:
         app.config["TESTING"] = True
