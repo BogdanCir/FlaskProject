59b1412067ad95c37cd428216b062e50afc2a4b9
BogdanCir <bogdancirciu1@gmail.com>
2026-04-01 18:45:32 +0300

    test deploy

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,10 @@
 from app import create_app
 from app.db import init_db
+import os
 
 init_db()
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
